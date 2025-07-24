a8d5da821b16cdef36524792dd3caebc05997826
Wujib97 <wyshanding@gmail.com>
2025-07-24 16:49:07 +0100

    solution to task4

--- task4.py
+++ task4.py
@@ -10,3 +10,19 @@
     4. Find and display the total number of courses the student is now registered for.
     5. The student wants to know what their 3rd course in the list is.
 """
+#task4 solution
+courses = ["MTH 101", "PHY 101", "CHM 101", "CSC 101", "GST 101"]
+courses.insert(0, "Eng 101")
+print(courses)
+courses.remove("GST 101")
+print(courses)
+courses.insert(4, "BIO 101")
+print(len(courses))
+print(courses[2])
+print(courses)
+
+
+
+
+
+
